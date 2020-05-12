b24b87215cab6369f7d2870fea36bae9d48f9029
Stephen Shamaiengar <s.shamaiengar@gmail.com>
2020-05-12 10:31:54 -0400

    Solved 4.1

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,9 +1,31 @@
 from test_framework import generic_test
 
+"""
+Ex.
+
+10011010 => 0
+10011000 => 1
+01100010 => 1
+
+Brute force: O(n) (n is length of binary word)
+Shift each bit off and count number of 1s
+
+For very large number of words, need to do better.
+Hint: Use a lookup table, but don't use 2^64 entries
+
+Use OR across halves? But this depends on position of 1s
+
+
+"""
 
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    # trick: x & (x-1) clears the lowest set bit in x
+    # (0100) & (0011) == 0
+    ct = 0
+    while x > 0:
+        x &= x - 1
+        ct += 1
+    return ct & 1
 
 
 if __name__ == '__main__':
