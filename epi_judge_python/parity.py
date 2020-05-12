41b1d97f0ba7e0ec36e12398b0f562a9d7140146
Stephen Shamaiengar <s.shamaiengar@gmail.com>
2020-05-12 11:15:30 -0400

    Solved 4.3

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -13,14 +13,25 @@ Shift each bit off and count number of 1s
 For very large number of words, need to do better.
 Hint: Use a lookup table, but don't use 2^64 entries
 
-Use OR across halves? But this depends on position of 1s
+Other solutions:
 
+Lookup table:
+For computing parity of many 64-bit words, can't precompute all results (2^64)
+BUT can precompute all parities for smaller subwords (4 16-bit subwords)
+2^16 is only 65536
+Precompute all and then go through subwords of each x, looking up parity and accumulating using XOR.
+
+XOR divide-and-conquer:
+Parity of x is parity of XOR of two halves of x
+Repeat down to last two bits and return result
+Time: O(log n)
 
 """
 
 def parity(x: int) -> int:
     # trick: x & (x-1) clears the lowest set bit in x
     # (0100) & (0011) == 0
+    # Time: O(k)
     ct = 0
     while x > 0:
         x &= x - 1
--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -1,9 +1,47 @@
 from test_framework import generic_test
 
+"""
+
+Ex.
+10010101 => 10101001
+
+Brute force: O(n)
+Shift each bit off and Shift it onto a new word
+OR swap each of least significant half of bits with most significant bits
+
+
+O(n/L)
+Lookup table? Store reversed versions of subwords. Lookup and combine
+
+
+"""
+
+def reverse_16(x):
+    res = 0
+    
+    for i in range(16):
+        bit = x & 1
+        res <<= 1
+        res |= bit
+        x >>= 1
+    return res
+
+# build lookup table of reversed 16-bit words
+revs = []
+for i in range(2**16):
+    revs.append(reverse_16(i))
 
 def reverse_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    mask = 0xffff
+    mask_size = 16
+    res = 0
+    for i in range(4):
+        subword = x & mask
+        x >>= mask_size
+        rev = revs[subword]
+        res <<= mask_size
+        res |= rev
+    return res
 
 
 if __name__ == '__main__':
