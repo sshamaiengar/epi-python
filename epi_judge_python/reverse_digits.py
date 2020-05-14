75329669fe94151bf12dd2747ae2c1408b5d4514
Stephen Shamaiengar <s.shamaiengar@gmail.com>
2020-05-14 10:36:24 -0400

    Solved 4.8

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -1,8 +1,28 @@
 from test_framework import generic_test
 
+"""
+
+Brute force: convert to string, reverse, convert back
+O(n) for n digits
+
+Faster:
+Use / and % to get each digit and add it to new number
+Match signs
+O(n) for n digits (lower multiple of n)
+
+"""
 
 def reverse(x: int) -> int:
-    # TODO - you fill in here.
+    sign = 1 if x >= 0 else -1
+
+    x = abs(x)
+    nx = 0
+    while x > 0:
+        d = x % 10
+        nx = nx * 10 + d
+        x //= 10
+    return nx * sign
+
     return 0
 
 
